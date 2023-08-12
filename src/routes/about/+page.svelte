66b3cb41ad03e3d8a294452aaa82b030a121bdcd
Rmaceac <ryan.maceachern.tech@gmail.com>
2023-08-12 00:29:45 -0400

    create new reviews and about page route, setup basic UI navigation

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,15 @@
+<div class='about'>
+  <h1>About Page</h1>
+  <p>This is the About page.</p>
+
+  <a href='/reviews'>See Reviews</a>
+<a href='/'>Home</a>
+</div>
+
+<style>
+  .about {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
