651c799c1a0694bf31fb2f2d73af296ba506afff
Rmaceac <ryan.maceachern.tech@gmail.com>
2023-08-13 22:00:02 -0400

    style home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,14 @@
-<h1>Welcome to The Casual Gamer Blog!</h1>
+<div class='home'>
+  <h1>Welcome to The Casual Gamer Blog!</h1>
+</div>
 
 <a href='/reviews'>See Reviews</a>
 <a href='/about'>About Us</a>
+
+<style>
+  .home {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
